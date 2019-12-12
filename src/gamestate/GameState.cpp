f6d77d5c93d03ffbb6b615b25c0726697a0a46c4
Anzurna <aversatrix@mail.ru>
2019-12-12 19:39:50 +0300

    added simple event listener for Player class

--- src/gamestate/GameState.cpp
+++ src/gamestate/GameState.cpp
@@ -1,6 +1,5 @@
 GameState::GameState(int width, int height)
 {
-
 	m_width = width;
 	m_height = height;
 	IsGameStateActive = true;
@@ -14,66 +13,40 @@ GameState::GameState(int width, int height)
 	parallaxView.zoom(0.3f);
 }
 
-void GameState::testFunct(sf::Event& evnt) {
-	if (evnt.type == sf::Event::KeyPressed && evnt.key.code ==  sf::Keyboard::R) {
-				Figure1.setPosition(585.0f, 282.0f);
-		}
-}
+
 
 void GameState::handle(sf::Event& evnt, sf::RenderWindow& window, /* sf::Vector2i& mousePos, */ GlobalContext& glob)
 {
 	sf::RectangleShape test(sf::Vector2f(40.0f, 40.0f));
-m_pathfinder.init(mapHandl);
-while (glob.getIsGameStateActive()) {
-	sf::Vector2i mousePos1 = sf::Mouse::getPosition(window);
-	while (window.pollEvent(evnt)) {
-
-
-
-/* 	if (evnt.type == sf::Event::KeyPressed && evnt.key.code ==  sf::Keyboard::R) {
-				Figure1.setPosition(585.0f, 282.0f);
-			} */
-	if (evnt.type == sf::Event::KeyPressed && evnt.key.code ==  sf::Keyboard::L) {
-				Figure1.setPosition(200.0f, 282.0f);
-
-				testFunct(evnt);
-	}
-	if (evnt.type == sf::Event::KeyPressed && evnt.key.code ==  sf::Keyboard::L) {
-				Figure1.setPosition(200.0f, 282.0f);
-			}
-	if (evnt.type == sf::Event::KeyPressed && evnt.key.code ==  sf::Keyboard::Escape) {
-				glob.setGameStateActive(false);
-			}
-
-
-
-	/* 	if (evnt.type == sf::Event::MouseButtonPressed && (evnt.mouseButton.button ==  sf::Mouse::Left)
-		&& sf::Keyboard::isKeyPressed(sf::Keyboard::Key:: M)) {
-			for ( unsigned int i = 0; i < allVertex.size(); i++ ) {
-				if (allVertex[i].checkIsClicked(&window, mousePos, view)) {
-					allVertex[i].setPosition(mousePos);
+	m_pathfinder.init(mapHandl);
+	while (glob.getIsGameStateActive()) {
+		sf::Vector2i mousePos1 = sf::Mouse::getPosition(window);
+		while (window.pollEvent(evnt)) {
+			/* if (evnt.type == sf::Event::KeyPressed && evnt.key.code ==  sf::Keyboard::R) {
+						Figure1.setPosition(585.0f, 282.0f);
+					}
+			if (evnt.type == sf::Event::KeyPressed && evnt.key.code ==  sf::Keyboard::L) {
+						Figure1.setPosition(200.0f, 282.0f);
+					} */
+			if (evnt.type == sf::Event::KeyPressed && evnt.key.code ==  sf::Keyboard::Escape) {
+						glob.setGameStateActive(false);
 				}
-
+			if (evnt.type == sf::Event::KeyPressed && evnt.key.code ==  sf::Keyboard::Tilde) {
+				for (unsigned int i = 0; i < mapHandl.allVertex.size(); i++) {
+					mapHandl.allVertex[i].toggleVisibility();
+				}
 			}
-		} */
-		if (evnt.type == sf::Event::KeyPressed && evnt.key.code ==  sf::Keyboard::Tilde) {
-			for (unsigned int i = 0; i < mapHandl.allVertex.size(); i++) {
-				mapHandl.allVertex[i].toggleVisibility();
+			if (evnt.type == sf::Event::KeyPressed && evnt.key.code ==  sf::Keyboard::P) {
+				ToggleParallax = (ToggleParallax ? false : true);
 			}
+		/* 	if (evnt.type == sf::Event::MouseButtonReleased && (evnt.mouseButton.button ==  sf::Mouse::Right)) {
+				Figure1.moveToVertex( window, mapHandl, mousePos1, view, m_pathfinder);
+			} */
+
+			Figure1.eventListener(evnt, window, mapHandl, mousePos1, view, m_pathfinder);
 		}
-		if (evnt.type == sf::Event::KeyPressed && evnt.key.code ==  sf::Keyboard::P) {
 
-			ToggleParallax = (ToggleParallax ? false : true);
-		}
-
-		if (evnt.type == sf::Event::MouseButtonReleased && (evnt.mouseButton.button ==  sf::Mouse::Right)) {
-
-			Figure1.moveToVertex( window, mapHandl, mousePos1, view, m_pathfinder);
-
-		}
-	}
-
-			if (mousePos1.x > m_width - 10) { // Перемещение видов, позже бует вынесено в отдельный объект
+		if (mousePos1.x > m_width - 10) { // Перемещение видов, позже бует вынесено в отдельный объект
 			view.move(5.0f, 0.0f);
 			parallaxView.move(0.3f, 0.0f);
 
@@ -90,13 +63,11 @@ while (glob.getIsGameStateActive()) {
 			parallaxView.move(0.0f, 0.3f);
 		}
 
-
 		//infotable.showInfo(&window, &player, mousePos,  Figure1.getTargX(), Figure1.getTargY(),
 					//	   Figure1.getPreviousX(), Figure1.getPreviousY());
 		window.clear();
 		window.setView(parallaxView);
 		if (ToggleParallax) { mapHandl.drawParallax(&window); }
-
 
 		window.setView(view);
 		mapHandl.drawMap(&window);
@@ -107,7 +78,6 @@ while (glob.getIsGameStateActive()) {
 			mapHandl.allVertex[i].draw(&window, view);
 		}
 		window.display();
-
 	}
 }
 
